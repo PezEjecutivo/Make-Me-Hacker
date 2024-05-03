c6cc8a75899489db14ddebd6f09f98d725b52015
PezEjecutivo <waterlolbusiness@hotmail.com>
2024-05-03 23:28:58 +0200

    Un pelin de diseño

--- resources/views/registro/layouts.blade.php
+++ resources/views/registro/layouts.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>CRUD Basico Enemigos TBOI Repentance</title>
+    <title>CRUD Basico Make me hacker!</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="/css/style.css">
@@ -16,7 +16,7 @@
 <body>
 
     <div class="container">
-        <h3 class=" mt-3">CRUD Basico Enemigos TBOI Repentance</h3>
+        <h3 class=" mt-3">CRUD Basico Make me hacker!</h3>
         @yield('content')
     </div>
 
